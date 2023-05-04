e03fc17729f2db37b97929d798ed867d4535109e
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-05-04 23:04:35 +0800

    finish rater and main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from collections import defaultdict
 import os
 from extractor import Extractor
 from rater import Rater
+from multiprocessing import Pool
+import time
 
 FILEPATH = os.path.join('data','data.csv')
+OUTPATH = os.path.join('out','result.csv')
 PRODUCT_LABELS = ['reviewerID',
                   'asin',
                   'reviewerName',
@@ -13,14 +17,55 @@ PRODUCT_LABELS = ['reviewerID',
                   'unixReviewTime',
                   'reviewTime']
 
+RATING_CRITERIAS = ["Product quality",
+                  "Difficulty using the product",
+                  "Affordability",
+                  "Cable / accessories quality",
+                  "Sound quality",
+                  "On-time delivery",
+                  "Ease to return product",
+                  "Returning customer satisfaction",
+                  "Product appearance",
+                  "Ease to reach customer service",
+                  "Will purchase again",
+                  "Adjustability",
+                  "Good quality for the price",
+                  "Package integrity"]
+
 def main():
   extractor = Extractor()
-  rater = Rater(product_data)
+  rater = Rater()
 
   products = extractor.extract_data(FILEPATH)
-  rater.rate_product()
-  ratings = rater.get_ratings()
-  print(ratings)
+  ratings = defaultdict(list)
+
+  t0 = time.time()
+
+  with Pool(10) as p:
+    ratings = p.map(rater.rate_product, products)
+
+  t1 = time.time()
+
+  print(f'Finished benchmarking {len(products)} reviews in {t1-t0} seconds')
+
+  result  = defaultdict(list)
+  for rating in ratings:
+    for k,v in rating.items():
+      result[k] = v
+
+  print(f'Writing result to {OUTPATH}')
+  with open(FILEPATH,"r") as fobj:
+    with open(OUTPATH,"w+") as outfile:
+      outfile.write(",".join(PRODUCT_LABELS) + "," + ",".join(RATING_CRITERIAS) + ",\n")
+      for line in fobj:
+        if line.split(',')[0] == PRODUCT_LABELS[0]:
+          continue
+        if not result or line.split(',')[0] not in result:
+          continue
+
+        outfile.write(line.strip() + ",".join(
+          list(map(str,result[line.split(',')[0]]))
+          ) + ",\n")
 
 if __name__ == "__main__":
   main()
--- src/rater.py
+++ src/rater.py
@@ -1,29 +1,24 @@
+import os
+
 import openai
+from dotenv import load_dotenv
+
 from product import Product
 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
 
 class Rater:
   MIN_RATING = -5
   MAX_RATING = 5
   START_PROMPT = f"""
-  You are GPT, a large language model trained by OpenAI.
-  You will be given texts of consumer reviews of specific products purchased on Amazon.
-  You will be given 1 review at a time, for each review, You must translate the verbal review into a rating ranging from {MIN_RATING} to {MAX_RATING} on each of the following criterias.
-  1. Product quality
-  2. Difficulty using the product
-  3. Affordability
-  4. Cable / accessories quality
-  5. Sound quality
-  6. On-time delivery
-  7. Ease to return product
-  8. Returning customer satisfaction
-  9. Product appearance
-  10. Ease to reach customer service
-  11. Will purchase again
-  12. Adjustability
-  13. Good quality for the price
-  14. Package integrity
-  You will give me the rating in this exact format:
+  I am a product review benchmarker.
+  I will be given texts of consumer reviews of musical instruments purchased on Amazon.
+  I will be given 1 review at a time, for each review, I must translate the verbal review into a rating ranging from {MIN_RATING} to {MAX_RATING}, where a positive 5 means extremely positive experience; a negative 5 indicates extremely negative experience. If the review does not mention anything on that criteria, the rating is 0.
+  on each of the 14 following criterias,
+  while keeping in mind that the review is about a music instrument.
+  I will give respond the rating in this exact format:
   1. Product quality: <rating>
   2. Difficulty using the product: <rating>
   3. Affordability: <rating>
@@ -40,14 +35,35 @@ class Rater:
   14. Package integrity: <rating>
   """
 
-  def __init__(self,product_data:Product):
-    openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[{"role": "user", "content": self.START_PROMPT}]
-      )
+  def __init__(self):
+    self.start_prompt = {"role": "user", "content": self.START_PROMPT}
 
-    self.product_data = product_data
+  def parse_review(self,review:str):
+    review = review.split("\n")
+    ratings = []
+    for line in review:
+      try:
+        rating = line.split(":")[1].strip()
+      except:
+        raise ValueError(f'Could not parse rating from line: {line}, bad input?')
+      ratings.append(int(rating))
 
-  def rate_product(product:Product):
-    rating = 0
-    return rating
+    return ratings
+
+  def rate_product(self, product:Product):
+    print(f'Rating review from reviewerId: {product.reviewerID}')
+    try:
+      response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+          self.start_prompt,
+          {"role":"user","content":product.reviewText}
+          ]
+        )
+
+      review = response["choices"][0]["message"]["content"]
+
+      return {product.reviewerID:self.parse_review(review)}
+
+    except:
+      return {product.reviewerID:[]}
